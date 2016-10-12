7b555469c9df06484644782aa908c7c25155ed6a
Jeremy Michael Cerda <jcerda@freighthousemedia.com>
2016-10-12 02:30:06 -0700

    .

---
+++ js/sunrun.js
@@ -0,0 +1,16 @@
+// Find all iframes
+var $iframes = $( "iframe" );
+
+$iframes.each(function () {
+  $( this ).data( "ratio", this.height / this.width )
+    .removeAttr( "width" )
+    .removeAttr( "height" );
+});
+
+$( window ).resize( function () {
+  $iframes.each( function() {
+    var width = $( this ).parent().width();
+    $( this ).width( width )
+      .height( width * $( this ).data( "ratio" ) );
+  });
+}).resize();
